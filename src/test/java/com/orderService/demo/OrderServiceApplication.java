3e465266337f2e47e5b8b44b37c02fd46806ad6d
AnishPatel115 <anish.p@brilworkss.com>
2023-12-12 18:31:02 +0530

    first commit

---
+++ src/test/java/com/orderService/demo/OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.orderService.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplication {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
